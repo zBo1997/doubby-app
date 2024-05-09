a2cf77b3a99ab54fe906c0ec407f929008897ecf
zhubo <zhubo@vv.cn>
2024-05-09 10:25:07 +0800

    喜好界面

--- src/views/index/like.vue
+++ src/views/index/like.vue
@@ -2,12 +2,24 @@
 <template>
     <div class="lide">
         <van-cell title="设置喜好" is-link @click="showPopup"></van-cell>
-        <van-popup
+        <van-popup             
             v-model:show="show"
+            round
             closeable
+            close-icon-position="top-left"
+            close-icon="arrow-down"
             position="bottom"
-            :style="{ height: '30%' }"
-        />
+            :style="{ height: '50%' }"
+            >
+            <div class="like-list">
+                <h4>选择你的喜好</h4>
+                <p>请选择喜欢的分类：已经选择 {{}} 个</p>
+                <ul>
+                    <li v-for="(v,i) in like_list">{{ v.name }}</li>
+                </ul>
+            </div>
+        
+        </van-popup>
     </div>
 
 </template>
@@ -19,6 +31,16 @@ const showPopup = () => {
     show.value = true;
 };
 
+const like_list = ref([
+    {name:'都市',id:1},
+    {name:'科幻',id:2},
+    {name:'体育',id:3},
+    {name:'历史',id:4},
+    {name:'教育',id:5},
+    {name:'游戏',id:6},
+    {name:'外卖',id:7},
+    {name:'向导',id:8},
+])
 
 
 </script>
